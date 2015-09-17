4422b0133b78fd2d101e22cff2b27dace50a9d34
Lakshin <lakshinkarunaratne@gmail.com>
2015-09-17 11:12:08 +0530

    Added file generating and closing app in few seconds

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 
@@ -6,6 +7,28 @@ app.get('/', function(req, res) {
 	res.send('hello world');
 });
 
-app.listen(process.env.PORT || 5000);
+var server = app.listen(process.env.PORT || 5000);
 console.log('App started on port 5000');
 module.exports = app;
+
+var fileData = '<?xml version="1.0" encoding="UTF-8"?>'+
+'<testsuites name="Mocha Tests" timestamp="2015-09-15T08:59:26" time="0.032" tests="1" failures="1">'+
+  '<testsuite name="GET /" timestamp="2015-09-15T08:59:26" tests="1" failures="1" time="0.032">'+
+    '<testcase name="GET / respond with hello world" time="0.032" className="respond with hello world">'+
+      '<failure>expected &apos;hello world&apos; response body, got &apos;hello Testing&apos;</failure>'+
+    '</testcase>'+
+  '</testsuite>'+
+'</testsuites>';
+
+//file write
+fs.writeFile('test.xml', fileData, function (err) {
+  if (err) return console.log(err);
+  console.log('Test report generated');
+});
+
+//close connection
+setTimeout(function() {
+	console.log("yes");
+	server.close();
+		
+},1000*10);
